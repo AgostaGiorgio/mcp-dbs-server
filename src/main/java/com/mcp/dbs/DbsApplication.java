f6a84db6a495b3359b95d214516aaaabb868bf8c
AgostaGiorgio <agogi.dev@gmail.com>
2025-05-28 15:34:06 +0400

    add neo4j config + check credentials

--- src/main/java/com/mcp/dbs/DbsApplication.java
+++ src/main/java/com/mcp/dbs/DbsApplication.java
@@ -2,8 +2,11 @@ package com.mcp.dbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class DbsApplication {
 
 	public static void main(String[] args) {
